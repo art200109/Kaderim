2a4bef43509bc72d37e3e1dc0d2291efbb300c31
Artume Kolesnikov <Artuem@New.Comp>
2022-11-15 14:46:51 +0200

    change

--- login/app.py
+++ login/app.py
@@ -7,7 +7,7 @@ from bson import json_util, ObjectId
 import json
 
 client = MongoClient()
-client = MongoClient('mongodb-36-rhel7', 27017)
+client = MongoClient('mongodb-36-rhel7.kaderim.svc.cluster.local', 27017)
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
