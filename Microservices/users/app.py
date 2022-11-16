020d44f51e5b019c581c29069c5d7d45190fff6b
RuBublik <relik@sweethome.local>
2022-11-16 12:23:05 +0200

    added users route /girls

--- Microservices/users/app.py
+++ Microservices/users/app.py
@@ -26,7 +26,7 @@ def bar ():
 @app.route("/girls")
 def get_girls():
     print (help(client.kader.users.find))
-    return parse_mongo(client.kader.users.find(gender=1))
+    return parse_mongo(client.kader.users.find({"gender":1}))
 
 @app.route("/<user_id>")
 def login(user_id):
